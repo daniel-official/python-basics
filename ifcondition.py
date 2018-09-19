d6db79f78ddc2d77aabec38c2943abffeb10fd64
daniel <40943662+daniel-official@users.noreply.github.com>
2018-09-19 16:21:42 +0530

    Update ifcondition.py

--- ifcondition.py
+++ ifcondition.py
@@ -5,9 +5,9 @@ age=input()
 print ("Hi "+name+" you're "+age+" years old" )
 if int(age)<18:
     print ("you're not eligible for voting")
-    diff=int(age)-18
-    print ("you can vote after "+diff+" years")
+    diff=18-int(age)
+    print ("you can vote after ",diff," years")
 else:
     print ("you're eligible for voting")
 
-    
+
